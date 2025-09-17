402b5b489c3bc74f3c75c50298f084037e85d0f6
Никита Шидогубов <mered1an@live.com>
2025-09-17 13:59:04 +0300

    [~] Мелкие фиксы, в основном удаление докстрингов, логическое перемещение файлов. Тесты и вьюхи пока не трогаю, возможно понадобятся на доработку, в текущем исполнении не используются

--- tests2/apps.py
+++ tests2/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class Tests2Config(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "tests2"
+    verbose_name = "2 Тестовое задание"
 
     def ready(self):
         pass
